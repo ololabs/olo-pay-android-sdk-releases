50a03fe66f32f8d399cf92ad07baaa852915a2cf
runner <runner@Mac-1664370847860.local>
2022-09-28 13:34:51 +0000

    Prepare for v1.2.2

--- documentation/scripts/sourceset_dependencies.js
+++
@@ -1,1 +0,0 @@
-sourceset_dependencies='{":OloPaySDK:dokkaHtml/Dev":[],":OloPaySDK:dokkaHtml/DevDebug":[],":OloPaySDK:dokkaHtml/DevRelease":[],":OloPaySDK:dokkaHtml/Prod":[],":OloPaySDK:dokkaHtml/ProdDebug":[],":OloPaySDK:dokkaHtml/ProdRelease":[],":OloPaySDK:dokkaHtml/androidTestRelease":[],":OloPaySDK:dokkaHtml/debug":[],":OloPaySDK:dokkaHtml/main":[],":OloPaySDK:dokkaHtml/release":[]}'
