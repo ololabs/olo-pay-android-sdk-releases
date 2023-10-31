c38b292a593997855aaba1a732625b9d32158bed
runner <runner@Mac-1698711349323.local>
2023-10-31 01:00:33 +0000

    Prepare for v3.0.0

--- src/TestHarness/src/main/java/com/olo/olopaytestharness/models/WorkerStatus.kt
+++
@@ -1,9 +0,0 @@
-// Copyright © 2022 Olo Inc. All rights reserved.
-// This software is made available under the Olo Pay SDK License (See LICENSE.md file)
-package com.olo.olopaytestharness.models
-
-import kotlinx.coroutines.flow.MutableStateFlow
-
-class WorkerStatus : IWorkerStatus {
-    override val isBusy = MutableStateFlow(false)
-}
