bfb37f84691ba8e713792c5fd28be4ad11cf7c0a
runner <runner@Mac-1711067781888.local>
2024-03-22 01:05:47 +0000

    Prepare for v3.1.0

--- src/OloPaySDK/src/main/java/com/olo/olopay/internal/callbacks/PostalCodeValidListener.kt
+++
@@ -1,7 +0,0 @@
-// Copyright © 2022 Olo Inc. All rights reserved.
-// This software is made available under the Olo Pay SDK License (See LICENSE.md file)
-package com.olo.olopay.internal.callbacks
-
-internal fun interface PostalCodeValidListener {
-    fun onPostalCodeValid()
-}
