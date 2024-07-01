ee9a1dcb8f52f392fd797dd7d2271e4d8154dabd
Anka <runner@Mac-1719872687599.local>
2024-07-01 22:40:10 +0000

    Initial SDK docs commit

---
+++ versions/1.0.1/scripts/sourceset_dependencies.js
@@ -0,0 +1,1 @@
+sourceset_dependencies='{":OloPaySDK:dokkaHtml/Dev":[],":OloPaySDK:dokkaHtml/DevDebug":[],":OloPaySDK:dokkaHtml/DevRelease":[],":OloPaySDK:dokkaHtml/Prod":[],":OloPaySDK:dokkaHtml/ProdDebug":[],":OloPaySDK:dokkaHtml/ProdRelease":[],":OloPaySDK:dokkaHtml/androidTestRelease":[],":OloPaySDK:dokkaHtml/debug":[],":OloPaySDK:dokkaHtml/main":[],":OloPaySDK:dokkaHtml/release":[]}'
