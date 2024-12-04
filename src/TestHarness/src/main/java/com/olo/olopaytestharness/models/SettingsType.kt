caa748d1fa40d11fe0305104e96c817ac8770301
Anka <runner@Mac-1733279948038.local>
2024-12-04 02:59:17 +0000

    Prepare for 3.1.2

--- src/TestHarness/src/main/java/com/olo/olopaytestharness/models/SettingsType.kt
+++
@@ -1,9 +0,0 @@
-// Copyright © 2022 Olo Inc. All rights reserved.
-// This software is made available under the Olo Pay SDK License (See LICENSE.md file)
-package com.olo.olopaytestharness.models
-
-enum class SettingsType(val description: String) {
-    CreditCard("CreditCard"),
-    GooglePay("GooglePay"),
-    CvvToken("CvvToken");
-}
